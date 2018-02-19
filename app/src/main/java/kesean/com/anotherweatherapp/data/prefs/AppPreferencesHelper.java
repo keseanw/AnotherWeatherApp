8fb4fd44e8ee94a4e03911946f23baac57d6d995
keseanw <keseanwilson3@gmail.com>
2018-02-18 23:49:38 -0500

    shared pref set up

---
+++ app/src/main/java/kesean/com/anotherweatherapp/data/prefs/AppPreferencesHelper.java
@@ -0,0 +1,17 @@
+package kesean.com.anotherweatherapp.data.prefs;
+
+/**
+ * Created by Kesean on 2/18/18.
+ */
+
+public class AppPreferencesHelper implements PreferencesHelper {
+    @Override
+    public void setWeatherCityName(String cityName) {
+        
+    }
+
+    @Override
+    public String getWeatherCityName() {
+        return null;
+    }
+}
---
+++ app/src/main/java/kesean/com/anotherweatherapp/data/prefs/PreferencesHelper.java
@@ -0,0 +1,12 @@
+package kesean.com.anotherweatherapp.data.prefs;
+
+/**
+ * Created by Kesean on 2/18/18.
+ */
+
+public interface PreferencesHelper {
+
+    void setWeatherCityName(String cityName);
+
+    String getWeatherCityName();
+}
